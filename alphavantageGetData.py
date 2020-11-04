f12618476821723bc57efcfd227cc66d91be3f94
ahmed ben ali <58703693+ahmed-benali@users.noreply.github.com>
2020-11-04 20:03:45 +0100

    Update alphavantageGetData.py

--- alphavantageGetData.py
+++ alphavantageGetData.py
@@ -20,10 +20,10 @@ data_ts, meta_data_ts = ts.get_intraday(symbol=my_symbol, interval='60min', outp
 period = 60
 
 ti = TechIndicators(key=api_key, output_format='pandas')
-data_ti, meta_data_ti = ti.get_sma(symbol=my_symbol, interval='60min', time_period=peroid, series_type='close')
+data_ti, meta_data_ti = ti.get_sma(symbol=my_symbol, interval='60min', time_period=period, series_type='close')
 
 df1 = data_ti
-df2 = data_ts['4. close'].iloc[peroid-1::]
+df2 = data_ts['4. close'].iloc[period-1::]
 
 
 df2.index = df1.index
